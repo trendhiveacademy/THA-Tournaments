92ae04d2888afa4d662f62ad4a9deca2e7f47ff5
trendhiveacademy <trendhiveacademy@gmail.com>
2025-06-27 11:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ app = Flask(__name__, template_folder='templates') # Explicitly specify template
 # IMPORTANT: Replace 'YOUR_SUPER_SECRET_KEY' with a strong, random, and unique secret key.
 # This is crucial for Flask session security. Generate a long, random string.
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'a_very_long_and_complex_random_string_for_dev_purposes_change_this_in_prod_really_change_it')
-CORS(app) # Enable CORS for all routes. Adjust origins/methods as needed for production.
-
+#CORS(app) # Enable CORS for all routes. Adjust origins/methods as needed for production.
+CORS(app, resources={r"/api/*": {"origins": "https://trendhiveacademy.github.io"}})
 # =====================================================================
 # FIREBASE INITIALIZATION
 # =====================================================================
